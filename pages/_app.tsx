4169cb18c32143dcc5b137077f62cc139a46ae49
John Apostol <notimetobeidle@gmail.com>
2021-06-17 22:53:54 -0500

    Add devDeps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-const MyApp = ({Component, pageProps}) =>
-  <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 
-export default MyApp
+export default MyApp;
