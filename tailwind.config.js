8cbd7b364e1c3affbf076a9ac978b9b78f08186f
John Apostol <notimetobeidle@gmail.com>
2021-07-10 20:53:56 -0500

    Mess with it all

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+<<<<<<< HEAD
     extend: {},
+=======
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+        heading: ["Open Sans", "sans-serif"],
+      },
+    },
+>>>>>>> Mess with it all
   },
   variants: {
     extend: {},
